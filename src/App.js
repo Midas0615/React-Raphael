6d5e0e27a25fc5cab78b66fa6437cd7c6f1cafe9
midas0615 <marekjohn006@gmail.com>
2019-01-20 01:08:06 -0800

    change the text

--- src/App.js
+++ src/App.js
@@ -23,12 +23,12 @@ class App extends React.Component{
                       }
                       </Set>
                       <Set>
-                          {/* <Rect x={30} y={148} width={240} height={150} attr={{"fill":"#10a54a","stroke":"#f0c620","stroke-width":5}}/>
+                          <Rect x={30} y={148} width={240} height={150} attr={{"fill":"#10a54a","stroke":"#f0c620","stroke-width":5}}/>
                           <Ellipse x={150} y={198} ry={40} rx={100} attr={{"fill":"#fff","stroke":"#e11032"}} glow={{width:10,fill:true,color:"#0b8ac9",opacity:1}}/>
                           <Image src="static/images/5circle.png" x={100} y={170} width={90} height={60} />
-                          <Text x={150} y={258} text="同一个世界 同一个梦想" attr={{"fill":"#fff"}}/>
+                          <Text x={150} y={258} text="One world One Dream" attr={{"fill":"#fff"}}/>
                           <Text x={150} y={273} text="One World One Dream" attr={{"fill":"#fff"}}/>
-                          <Path d={["M150 287L150 287"]} animate={Raphael.animation({"path": ["M80 287L220 287"]},500,"<>")} attr={{"stroke":"#fff"}}/> */}
+                          <Path d={["M150 287L150 287"]} animate={Raphael.animation({"path": ["M80 287L220 287"]},500,"<>")} attr={{"stroke":"#fff"}}/>
                           <Line x1={150} y1={290} x2={150} y2={290} animate={Raphael.animation({ x1:80, x2:220},500,"<>")} attr={{"stroke":"#fff"}}/>
                       </Set>
               </Paper>)
